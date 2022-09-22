5ceb3f2b8bccd5f2f1a0bfe69cc8aef6ccc4b816
hgulese <hgulese02@gmail.com>
2022-09-22 17:04:42 +0300

    deneme

---
+++ main.js
@@ -0,0 +1,19 @@
+function getTime() {
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+
+    (hour < 10) ? document.getElementById("hour").innerText = "0" + hour :
+        document.getElementById("hour").innerText = + hour;
+
+    (minute < 10) ? document.getElementById("minute").innerText = "0" + minute :
+        document.getElementById("minute").innerText = minute;
+
+    (second < 10) ? document.getElementById("second").innerText = "0" + second :
+        document.getElementById("second").innerText = second;
+}
+
+setInterval(function () {
+    getTime();
+}, 1000);
